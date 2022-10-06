d2ba81db53046d8b35a27100bc174f19bb13bfab
Adam-Isaak-dev <67387234+Adam-Isaak-dev@users.noreply.github.com>
2022-10-06 17:40:32 -0500

    Add Header data and remove preinstalled images

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import createEmotionCache from '../utility/createEmotionCache';
@@ -18,6 +19,15 @@ const MyApp = (props) => {
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <Head>
+          <title>Random Dad Joke&apos;s</title>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="apple-touch-icon" sizes="240x240" href="/icon-240.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icon-32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icon-16.png" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        </Head>
         <TopBar />
         <Component {...pageProps} />
         <BottomBar />
